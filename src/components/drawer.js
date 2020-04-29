f741324b3cac0d86f10aad5346beacfb5aae2ce0
Bryan Oemar <bryan.oemar@gmail.com>
2020-04-30 00:53:11 +0200

    [Drawer component] starting with the persistent drawer option
    This makes it a bit difficult, since the childs have fixed placed in the drawer component itself.
    I'm not yet able to figure out how to do this without using a 'hacky' way like assuming that the last content will always be the column component where the content will be in.

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -7,7 +7,66 @@
   jsx: (
     <div className={classes.panel}>
       {(() => {
+        const { makeStyles, useTheme } = window.MaterialUI.Core;      
 
+        const drawerWidth = 240;
+        const useStyles = makeStyles(theme => ({
+          root: {
+            display: 'flex',
+          },
+          appBar: {
+            transition: theme.transitions.create(['margin', 'width'], {
+              easing: theme.transitions.easing.sharp,
+              duration: theme.transitions.duration.leavingScreen,
+            }),
+          },
+          appBarShift: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            transition: theme.transitions.create(['margin', 'width'], {
+              easing: theme.transitions.easing.easeOut,
+              duration: theme.transitions.duration.enteringScreen,
+            }),
+          },
+          menuButton: {
+            marginRight: theme.spacing(2),
+          },
+          hide: {
+            display: 'none',
+          },
+          drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+          },
+          drawerPaper: {
+            width: drawerWidth,
+          },
+          drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 1),
+            // necessary for content to be below app bar
+            ...theme.mixins.toolbar,
+            justifyContent: 'flex-end',
+          },
+          content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+            transition: theme.transitions.create('margin', {
+              easing: theme.transitions.easing.sharp,
+              duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: -drawerWidth,
+          },
+          contentShift: {
+            transition: theme.transitions.create('margin', {
+              easing: theme.transitions.easing.easeOut,
+              duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+          },
+        }))
+        
         const { env } = B
         const { Drawer, 
                 Button, 
@@ -15,11 +74,20 @@
                 Divider, 
                 ListItem, 
                 ListItemIcon, 
-                ListItemText 
+                ListItemText,
+                AppBar,
+                CssBaseline,
+                Toolbar,
+                IconButton,
+                Typography,
               } = window.MaterialUI.Core;
 
-              
-        const { Inbox, Mail } = window.MaterialUI.Icons
+
+        const { Menu: MenuIcon,                
+                ChevronLeft: ChevronLeftIcon,
+                ChevronRight: ChevronRightIcon,
+                Inbox: InboxIcon,
+                Mail: MailIcon } = window.MaterialUI.Icons
 
         const isDev = env === 'dev';
 
@@ -36,14 +104,37 @@
         }
         
         const [state, setState] = useState(initialState);
+        const [open, setOpen] = useState(isOpenOnStart)
+
+        const _classes = useStyles();
+        const _theme = useTheme();
 
         useEffect(() => {
           B.defineFunction("handleOpenDrawer", e => {
             e.preventDefault();
         
-            setState({ ...state, [anchor]: true });
+            handleOpenDrawer(e, anchor)
+          });
+
+          B.defineFunction("handleCloseDrawer", e => {
+            e.preventDefault();
+        
+            handleCloseDrawer(e)
           });
         }, []);
+
+        const handleOpenDrawer = (e, anchor) => {
+          setState({ ...state, [anchor]: true })
+          setOpen(true)
+        }
+
+        const handleCloseDrawer = e => {
+          setState({ top: false,
+                    left: false,
+                    bottom: false,
+                    right: false, })
+          setOpen(false)
+        }
 
         const toggleDrawer = (anchor, open) => (event) => {
           if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -53,46 +144,89 @@
           setState({ ...state, [anchor]: open });
         };
 
-        const list = (anchor) => (
-          <div
-            className={clsx(classes.list, {
-              [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-            })}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-          >
-            <List>
-              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <Inbox /> : <Mail />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-            <List>
-              {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem button key={text}>
-                  <ListItemIcon>{index % 2 === 0 ? <Inbox /> : <Mail />}</ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItem>
-              ))}
-            </List>
-          </div>
-        );
+        const contentIndex = React.Children.count(children) - 1;
 
-        const drawerComponent = (
-          <div>
-            {
-              <React.Fragment key={anchor}>
-                <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-                <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-                  {list(anchor)}
-                </Drawer>
-              </React.Fragment>
-            }
-          </div>
+        const contentChild  =   React.Children.map(children, (child, index) => {
+                                  if(index === contentIndex) return child;
+                                })
+
+        const otherChildren =   React.Children.map(children, (child, index) => {
+                                  if(index !== contentIndex) return child;
+                                })
+
+        const drawerComponentWithAppBar = (
+          <div className={_classes.root}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            className={clsx(_classes.appBar, {
+              [_classes.appBarShift]: open,
+            })}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={e => handleOpenDrawer(e, "left")}
+                edge="start"
+                className={clsx(_classes.menuButton, open && _classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap>
+                Persistent drawer
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={_classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+              paper: _classes.drawerPaper,
+            }}
+          >
+            <div className={_classes.drawerHeader}>
+              <IconButton onClick={handleCloseDrawer}>
+                {_theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+            <Divider />
+            {otherChildren}
+          </Drawer>
+          <main
+            className={clsx(_classes.content, {
+              [_classes.contentShift]: open,
+            })}
+          >
+            <div className={_classes.drawerHeader} />
+            {/* <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+              ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+              facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+              donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+              adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+              imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+              arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+              donec massa sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph>
+              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+              facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+              tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+              consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+              vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+              hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+              tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+              nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+              accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+            </Typography> */}
+            {contentChild}
+          </main>
+        </div>
         );
 
         const drawerComponentWithList = (
@@ -119,7 +253,7 @@
 
         return (
           <>
-            { isDev ? <div><p>Drawer</p>{children}</div> : drawerComponentWithList}
+            { isDev ? <div><p>Drawer</p>{children}</div> : drawerComponentWithAppBar}
           </>
         );
       })()}
